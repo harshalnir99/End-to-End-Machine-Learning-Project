14a366fcbceda2790efc22a4824a971005280cf0
Harshal Nirgulkar <harshalnir99@gmail.com>
2023-06-23 15:52:35 +0530

    model trainer completed

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -6,6 +6,7 @@ from src.exception import CustmeException
 from dataclasses import dataclass
 from sklearn.model_selection import train_test_split
 from src.components.data_tranformation import DataTransformation
+from src.components.model_trainer import ModelTrainer
 
 
 @dataclass
@@ -56,3 +57,6 @@ if __name__ =="__main__":
     data_transformation = DataTransformation()
     train_arr, test_arr, _ = data_transformation.inititate_data_transformation(train_data_path , test_data_path)
 
+    model_trainer = ModelTrainer()
+    print(model_trainer.inititate_model_trainer(train_arr, test_arr))
+    
